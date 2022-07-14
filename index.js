2fb746e7c1f5fc0ba265306d69cea99e3455666f
msunsmiley <msunsmiley@pm.me>
2022-07-13 21:24:09 -0400

    a

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ east.on('messageCreate', (message) => {
     // hey bot give me a goat pic
     else if(message.channelId === goatchannel.id){
         if(message.content.toLowerCase() === "hey bot give me a goat pic"){
-            let a = Math.floor(Math.random()*14) + 1;
+            let a = Math.floor(Math.random()*19);
             
             message.reply({files:['./img/goats/' + a + '.jpg']}).catch(console.error)
             console.log("goat pic given")
